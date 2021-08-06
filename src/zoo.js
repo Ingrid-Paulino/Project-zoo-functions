a4a0181c507659767488e490c42062f5bb73308e
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-08-06 11:23:37 -0300

    C7 - Adicionado Requisito 8 - calculateEntry

--- src/zoo.js
+++ src/zoo.js
@@ -1,15 +1,15 @@
-// const { species, employees } = require('./data');
+const { species, employees, prices, hours } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
   if (ids.length === 0) return [];
-  return ids.map((id) => data.species.find((animal) => animal.id === id));
+  return ids.map((id) => species.find((animal) => animal.id === id));
 }
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const animalSelected = data.species.find((animalElement) => animalElement.name === animal);
+  const animalSelected = species.find((animalElement) => animalElement.name === animal);
   return animalSelected.residents.every((elementAnimal) => elementAnimal.age >= age);
 }
 
@@ -18,7 +18,7 @@ function getEmployeeByName(employeeName) {
   // I placed the code more smaller for lint to stay happy
   const emp = employeeName;
   if (!emp) return {};
-  return data.employees.find((person) => person.firstName === emp || person.lastName === emp);
+  return employees.find((person) => person.firstName === emp || person.lastName === emp);
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -33,13 +33,13 @@ function createEmployee(personalInfo, associatedWith) {
     managers,
     responsibleFor,
   };
-  data.employees.push((newCollaborator));
+  employees.push((newCollaborator));
   return newCollaborator;
 }
 
 function isManager(id) {
   // seu código aqui
-  return data.employees.some((personCollaborator) => personCollaborator.managers.includes(id));
+  return employees.some((personCollaborator) => personCollaborator.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
@@ -51,16 +51,23 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
     managers,
     responsibleFor,
   };
-  data.employees.push(newObj);
+  employees.push(newObj);
   return newObj;
 }
 
-function countAnimals(especies) {
+function countAnimals(especiess) {
   // seu código aqui
+
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants) return 0;
+
+  const adult = prices.Adult * (entrants.Adult || 0);
+  const child = prices.Child * (entrants.Child || 0);
+  const senior = prices.Senior * (entrants.Senior || 0);
+  return adult + child + senior;
 }
 
 function getAnimalMap(options) {
@@ -77,6 +84,7 @@ function getOldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   // seu código aqui
+
 }
 
 function getEmployeeCoverage(idOrName) {
